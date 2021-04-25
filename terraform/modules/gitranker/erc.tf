5d57d7f41c89d78c65a2b4d447c436f75e44ed75
flow1981 <florian.stagliano@gmx.de>
2021-04-25 20:35:33 +0100

    Setup ECR repository

---
+++ terraform/modules/gitranker/erc.tf
@@ -0,0 +1,45 @@
+resource "aws_ecr_repository" "ecr_repo" {
+  name = "ecr-${var.app_name}"
+
+  tags = merge(local.default_tags,
+    {
+      Name = "ecr-${var.app_name}"
+    }
+  )
+}
+
+resource "aws_ecr_lifecycle_policy" "ecr_repo_policy" {
+  repository = aws_ecr_repository.ecr_repo.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep image deployed with tag latest",
+      "selection": {
+        "tagStatus": "tagged",
+        "tagPrefixList": ["latest"],
+        "countType": "imageCountMoreThan",
+        "countNumber": 1
+      },
+      "action": {
+        "type": "expire"
+      }
+    },
+    {
+      "rulePriority": 2,
+      "description": "Keep last 2 any images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 2
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
