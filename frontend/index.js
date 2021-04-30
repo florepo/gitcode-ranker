77610f040a31ab5c5a4e0589ee2a322f33a20f55
flow1981 <florian.stagliano@gmx.de>
2021-04-30 18:24:29 +0100

    Setup basic react frontend

--- frontend/index.js
+++
@@ -1,9 +0,0 @@
-console.log("test")
-fetch('http://api.cloudgate.link/flow1981', {
-  method: "GET",
-  body: JSON.stringify(),
-  headers: {"Content-type": "application/json; charset=UTF-8"}
-})
-.then(response => response.json()) 
-.then(json => console.log(json))
-.catch(err => console.log(err));
