2f9e6dd957648cd3a72470651f632e1f25f47740
flow1981 <florian.stagliano@gmx.de>
2021-04-30 18:23:06 +0100

    SSL ecs laod balancer

--- terraform/modules/cdn/certificate.tf
+++ terraform/modules/cdn/certificate.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   alias   = "acm_certificates_provider"
   region  = "us-east-1" # To use an ACM Certificate with CloudFront, we must request the certificate from the US East (N. Virginia) region.
-  version = "3.27"
 }
 
 data "aws_route53_zone" "root_domain" {
@@ -16,7 +15,7 @@ resource "aws_acm_certificate" "website_domain" {
   domain_name               = var.root_domain_name
   validation_method         = "DNS"
 
-  subject_alternative_names = ["*.${var.root_domain_name}"]
+  subject_alternative_names = ["www.${var.root_domain_name}", var.root_domain_name]
 
   lifecycle {
     create_before_destroy = true
