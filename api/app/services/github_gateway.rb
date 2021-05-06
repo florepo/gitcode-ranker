e9144d5c63b000aa02d2ad8afc8fa0db1aeae38e
flow1981 <florian.stagliano@gmx.de>
2021-05-06 18:44:10 +0100

    Clean up service models

--- api/app/services/github_gateway.rb
+++ api/app/services/github_gateway.rb
@@ -1,24 +1,20 @@
 class GithubGateway
+  def initialize
+    @access_token = Rails.application.credentials.dig(:github, :access_token)
+    @client = Octokit::Client.new(:access_token => @access_token)
+    @client.auto_paginate = true
+  end
 
-    def initialize
-        @access_token = Rails.application.credentials.dig(:github, :access_token)
-        @client = Octokit::Client.new(:access_token => @access_token)
-        @client.auto_paginate = true
+  def profile_exists(profile_name)
+    begin
+      user = @client.user profile_name
+      user.present? ? true : false
+    rescue
+      puts("profile not found")
     end
+  end
 
-    def profile_exists(profile_name)
-        begin
-            user = @client.user profile_name
-            user ? true : false
-        rescue
-            puts("profile does not exist")
-        end
-          
-    end
-
-    def get_profile_repos(profile_name)
-        repos = @client.repositories(profile_name, {sort: :pushed_at})
-        return repos
-    end
-
+  def get_profile_repos(profile_name)
+    @client.repositories(profile_name, {sort: :pushed_at})
+  end
 end
